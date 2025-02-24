be3553058b5247897e5e490d90278ba2635375fd
JNaor12 <guannaor@gmail.com>
2025-02-24 14:15:47 +0100

    Arreglo Inicio.tsx

--- src/Componentes/Paginas/Inicio.tsx
+++ src/Componentes/Paginas/Inicio.tsx
@@ -4,13 +4,13 @@ import ComponenteNavLink from "../ComponenteNavLink.tsx";
 
 const Inicio = () => {
   const [displayedTexts, setDisplayedTexts] = useState(["", "", ""]);
-  const fullTexts = [
-    "Naor Kadosh es un apasionado desarrollador de aplicaciones web con una insaciable curiosidad por aprender y crecer junto a sus compañeros de trabajo. Desde sus inicios en el mundo del desarrollo, ha demostrado un fuerte compromiso con la mejora continua y la adquisición de experiencia práctica.",
-    "Realizó sus prácticas en Unknown Gravity, donde tuvo la oportunidad de aplicar sus conocimientos y aprender de profesionales experimentados en el campo. Su enfoque colaborativo, resolutivo, su creatividad y su deseo de enfrentar nuevos desafíos lo han llevado a explorar diferentes aspectos del desarrollo web.",
-    "Con una sólida base en programación y diseño, Naor está listo para dar el siguiente paso en su carrera, buscando constantemente oportunidades para expandir sus habilidades y contribuir al éxito de su equipo."
-  ];
 
   useEffect(() => {
+    const fullTexts = [
+      "Naor Kadosh es un apasionado desarrollador de aplicaciones web con una insaciable curiosidad por aprender y crecer junto a sus compañeros de trabajo. Desde sus inicios en el mundo del desarrollo, ha demostrado un fuerte compromiso con la mejora continua y la adquisición de experiencia práctica.",
+      "Realizó sus prácticas en Unknown Gravity, donde tuvo la oportunidad de aplicar sus conocimientos y aprender de profesionales experimentados en el campo. Su enfoque colaborativo, resolutivo, su creatividad y su deseo de enfrentar nuevos desafíos lo han llevado a explorar diferentes aspectos del desarrollo web.",
+      "Con una sólida base en programación y diseño, Naor está listo para dar el siguiente paso en su carrera, buscando constantemente oportunidades para expandir sus habilidades y contribuir al éxito de su equipo.",
+    ];
     let currentParagraph = 0;
     let currentChar = 0;
 
@@ -19,7 +19,10 @@ const Inicio = () => {
         if (currentChar < fullTexts[currentParagraph].length) {
           setDisplayedTexts((prev) => {
             const newTexts = [...prev];
-            newTexts[currentParagraph] = fullTexts[currentParagraph].slice(0, currentChar + 1);
+            newTexts[currentParagraph] = fullTexts[currentParagraph].slice(
+              0,
+              currentChar + 1
+            );
             return newTexts;
           });
           currentChar++;
@@ -33,7 +36,7 @@ const Inicio = () => {
     };
 
     typeEffect();
-  }, []);
+  }, []); // Aquí agregamos fullTexts
 
   // Función para resaltar palabras en negrita
   const highlightWords = (text) => {
